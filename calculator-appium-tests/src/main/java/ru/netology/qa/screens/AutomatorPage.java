63ccda7112571aa856ae7915ea8b11f659a7b5f6
rimma test <valentin.rimma@gmail.com>
2023-12-19 11:31:40 +0400

    add finish hw

---
+++ calculator-appium-tests/src/main/java/ru/netology/qa/screens/AutomatorPage.java
@@ -0,0 +1,30 @@
+package ru.netology.qa.screens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+import java.time.Duration;
+public class AutomatorPage {
+    @AndroidFindBy(id = "ru.netology.testing.uiautomator:id/userInput")
+    public WebElement userInput;
+    @AndroidFindBy(id = "ru.netology.testing.uiautomator:id/buttonChange")
+    public WebElement buttonChange;
+    @AndroidFindBy(id = "ru.netology.testing.uiautomator:id/buttonActivity")
+    public WebElement buttonActivity;
+    @AndroidFindBy(id = "ru.netology.testing.uiautomator:id/text")
+    public WebElement text;
+
+
+
+    private AppiumDriver driver;
+
+    public AutomatorPage(AppiumDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+}
+
---
+++ calculator-appium-tests/src/test/java/ru/netology/qa/UIAutomatorTest.java
@@ -0,0 +1,80 @@
+package ru.netology.qa;
+
+import io.appium.java_client.android.AndroidDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.netology.qa.screens.AutomatorPage;
+
+public class UIAutomatorTest {
+    private AndroidDriver driver;
+    private final String emptyString = " ";
+    private String text = "vervte";
+
+    private AutomatorPage automatorPage;
+
+
+    @BeforeEach
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("appium:deviceName", "Some name");
+        desiredCapabilities.setCapability("appium:automationName", "uiautomator2");
+        desiredCapabilities.setCapability("appium:appPackage", "ru.netology.testing.uiautomator");
+        desiredCapabilities.setCapability("appium:appActivity", "ru.netology.testing.uiautomator.MainActivity");
+        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
+        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
+        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
+        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+
+
+        URL remoteUrl = new URL("http://127.0.0.1:4723/");
+
+        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+
+        automatorPage = new AutomatorPage(driver);
+    }
+
+
+    @Test
+    public void sampleTest() {
+        automatorPage.userInput.isDisplayed();
+        automatorPage.userInput.click();
+        automatorPage.userInput.sendKeys(emptyString);
+
+        automatorPage.buttonChange.isDisplayed();
+        automatorPage.buttonChange.click();
+
+        automatorPage.userInput.isDisplayed();
+        Assertions.assertEquals(emptyString,automatorPage.userInput.getText());
+    }
+    @Test
+    public void buttonActivityTest() {
+        automatorPage.userInput.isDisplayed();
+        automatorPage.userInput.click();
+        automatorPage.userInput.sendKeys(text);
+
+        automatorPage.buttonActivity.isDisplayed();
+        automatorPage.buttonActivity.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 1); // wait for up to 1 second
+        wait.until(ExpectedConditions.visibilityOf(automatorPage.text));
+
+        automatorPage.text.isDisplayed();
+
+        Assertions.assertEquals(text,automatorPage.text.getText());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+}
